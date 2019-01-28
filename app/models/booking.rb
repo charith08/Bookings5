b73546103b25e7bbcfc07dde8ddbd501b94f6681
charith08 <charithsaivadla@gmail.com>
2019-01-28 11:49:42 +0530

    statandendtimefixed

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,8 +3,15 @@ class Booking < ApplicationRecord
   belongs_to :room
   has_many :participants
   validates :name, presence: true, length: {maximum: 30}
-  validates :start_time, presence: true
+  validates :start_time, presence: true, if: :valids
   validates :end_time, presence: true, if: :valide
+
+
+  def valids
+    if (start_time < Time.current || start_time.to_time.hour<9 )
+      errors.add(:start_time)
+    end
+  end
 
 
 
